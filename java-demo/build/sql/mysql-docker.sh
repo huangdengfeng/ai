a902601ee1c65cccb0920a934d8e6ae4e7cf2fe4
root <root@workspace>
2026-05-20 14:56:14 +0800

    docker

---
+++ java-demo/build/sql/mysql-docker.sh
@@ -0,0 +1,4 @@
+# 安装
+docker run --name mysql8 --network host -e MYSQL_ROOT_PASSWORD=12345678 -d mysql:8.4.9
+# 导入 sql 文件 → 必须用 -i
+docker exec -i mysql8 sh -c 'exec mysql -uroot -p12345678' < /all.sql
